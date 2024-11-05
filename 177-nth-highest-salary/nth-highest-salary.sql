1629ee8db84e389b43aed71649fe82843cd2b771
Venkyy97 <159583041+Venkyy97@users.noreply.github.com>
2024-11-04 19:48:19 -0600

    Time: 2149 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,10 @@
+create function getnthhighestsalary(N int) returns int
+begin
+    set N = N - 1;
+    return(
+        select distinct salary from employee
+        order by salary desc
+        limit 1 offset N
+    );
+end 
+ 
