c53f634eb73dbb2fe62f223686d3b65422f1e09a
Venkyy97 <159583041+Venkyy97@users.noreply.github.com>
2024-11-20 11:04:19 -0600

    Time: 355 ms (95.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select distinct t1.seat_id  from cinema as t1, cinema as t2
+where t1.free = 1 and t2.free = 1 and 
+abs(t1.seat_id - t2.seat_id) = 1 
+order by seat_id asc ;
