995498ae022d781ca71331774cc38f86c0ec415c
Venkyy97 <159583041+Venkyy97@users.noreply.github.com>
2025-01-05 12:38:05 -0600

    Time: 859 ms (13.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1759-find-the-missing-ids/find-the-missing-ids.sql
@@ -0,0 +1,10 @@
+with recursive cte as (
+    select 1 as ids
+    union
+    select ids + 1 as ids
+    from cte 
+    where ids < (select max(customer_id) from customers)
+)
+select * from cte
+where ids not in (select customer_id from customers)
+order by ids asc
