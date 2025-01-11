264e63f5bcf46a2d2fad2ae2cf1536e390b633dc
Venkyy97 <159583041+Venkyy97@users.noreply.github.com>
2025-01-11 11:02:23 -0600

    Time: 848 ms (40.38%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
@@ -0,0 +1,16 @@
+with cte as (
+select 
+    case when from_id > to_id then to_id
+    else from_id end as person1,
+    case when from_id > to_id then from_id
+    else to_id end as person2, duration
+from calls
+)
+select person1, person2,
+count(duration) as call_count,
+sum(duration) as total_duration
+from cte group by 1,2
+
+
+
+
