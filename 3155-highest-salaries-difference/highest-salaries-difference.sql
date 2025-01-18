7248e9d6fb4c267a8e3b5f301d56ade3e4910e85
Venkyy97 <159583041+Venkyy97@users.noreply.github.com>
2025-01-18 10:54:08 -0600

    Time: 567 ms (33.08%) | Memory: 0B (100.00%) - LeetSync

--- 3155-highest-salaries-difference/highest-salaries-difference.sql
+++ 3155-highest-salaries-difference/highest-salaries-difference.sql
@@ -1,6 +1,4 @@
 
-SELECT ABS((SELECT MAX(salary) as salary FROM Salaries 
-WHERE department = "Engineering") 
-- (SELECT MAX(salary) as salary FROM Salaries
- WHERE department = "Marketing")) 
- AS salary_difference 
+select abs(max(case when department = 'Engineering' then salary else 0 end)
+            -max(case when department = 'Marketing' then salary else 0 end) ) as salary_difference 
+from salaries
