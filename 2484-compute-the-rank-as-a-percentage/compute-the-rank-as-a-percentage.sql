d505cb8c70b7663f10fd70ba80de5b7d71acf98f
Venkyy97 <159583041+Venkyy97@users.noreply.github.com>
2025-01-19 18:55:08 -0600

    Time: 1781 ms (17.07%) | Memory: 0B (100.00%) - LeetSync

--- 2484-compute-the-rank-as-a-percentage/compute-the-rank-as-a-percentage.sql
+++ 2484-compute-the-rank-as-a-percentage/compute-the-rank-as-a-percentage.sql
@@ -1,6 +1,14 @@
-SELECT student_id, department_id, 
-    ROUND(100*PERCENT_RANK() OVER (
-          PARTITION BY department_id 
-          ORDER BY mark DESC)
-    , 2) AS percentage 
-FROM Students
+# Write your MySQL query statement below
+with cte as (
+    select *,
+    rank() over (partition by department_id order by mark desc ) as student_rank_in_the_department,
+    count(student_id) over (partition by department_id) as  the_number_of_students_in_the_department
+    from students
+
+),
+cte2 as (
+    select student_id, department_id,
+    ifnull(round((student_rank_in_the_department - 1) * 100 / (the_number_of_students_in_the_department - 1), 2),0) as percentage
+    from cte
+)
+select * from cte2
