c72df0cb8ac4019795e8916000157eec5a55c30b
Venkyy97 <159583041+Venkyy97@users.noreply.github.com>
2025-01-26 13:06:41 -0600

    Time: 1058 ms (30.37%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,6 +1,8 @@
-with cte as (select employee_id, department_id, primary_flag,
-count(department_id) over (partition by employee_id) as cnt 
-from employee)
+with cte as (
+    select *,
+    count(department_id) over (partition by employee_id) as cnt
+    from employee
+)
 select employee_id, department_id
 from cte
 where cnt = 1 or primary_flag = 'Y'
