6bba2705f146481cbb1c8131990a40bddf0dfc65
Venkyy97 <159583041+Venkyy97@users.noreply.github.com>
2025-02-05 09:33:58 -0600

    Time: 1132 ms (12.45%) | Memory: 0B (100.00%) - LeetSync

--- 2370-users-with-two-purchases-within-seven-days/users-with-two-purchases-within-seven-days.sql
+++ 2370-users-with-two-purchases-within-seven-days/users-with-two-purchases-within-seven-days.sql
@@ -1,7 +1,7 @@
-# Write your MySQL query statement below
-select distinct p1.user_id
-from purchases p1, purchases p2
-where p1.user_id = p2.user_id
-and p1.purchase_id <> p2.purchase_id
-and abs(datediff(p2.purchase_date,p1.purchase_date)) <= 7
-order by user_id
+with cte as (select *,
+lag(purchase_date) over (partition by user_id order by purchase_date) as prev
+from purchases)
+
+select distinct user_id
+from cte
+where datediff(purchase_date,prev) <= 7
