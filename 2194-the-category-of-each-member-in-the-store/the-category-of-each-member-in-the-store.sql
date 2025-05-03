fef5952f8cf302e1268c852c8ecf8e0b04cebd42
Venkyy97 <159583041+Venkyy97@users.noreply.github.com>
2025-05-02 21:04:45 -0500

    Time: 1072 ms (98.05%) | Memory: 0B (100.00%) - LeetSync

--- 2194-the-category-of-each-member-in-the-store/the-category-of-each-member-in-the-store.sql
+++ 2194-the-category-of-each-member-in-the-store/the-category-of-each-member-in-the-store.sql
@@ -1,15 +1,29 @@
+
+with cte1 as (
 select
-    m.member_id,
-    name,
-    case 
-        when count(v.visit_id) = 0 then "Bronze"
-        when ifnull(count(distinct p.visit_id),0) * 100 / count(v.visit_id) >= 80 Then "Diamond"
-        when ifnull(count(distinct p.visit_id),0) * 100 / count(v.visit_id) >= 50 Then "Gold"
-        else 'Silver'
+m.member_id,
+m.name,
+v.visit_id,
+v.visit_date,
+p.charged_amount
+from Members m
+left join Visits v
+on m.member_id = v.member_id
+left join Purchases p
+on v.visit_id = p.visit_id
+order by member_id, visit_date
+),cte2 as (
+    select  member_id, name,
+    100 *(count(charged_amount)/count(distinct visit_id )) as score
+    from cte1 
+    group by member_id
+)
+select member_id, name,
+case 
+    when score is null then "Bronze"
+    when score >=80 then "Diamond"
+    when score >= 50 then "Gold"
+    else "Silver"
     end as category
-    from members m 
-    left join visits V using (member_id)
-    left join purchases p using (visit_id)
-    group by 1, 2
-    order by 1 asc 
-    
+from cte2
+order by 1 
