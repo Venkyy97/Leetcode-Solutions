0ea6b27d5bcb80fb05b2d5dadc1e2ea1672c2dbd
Venkyy97 <159583041+Venkyy97@users.noreply.github.com>
2025-08-04 22:41:59 -0500

    Time: 432 ms (87.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,14 @@
+with latest_prices as 
+(
+    select *,
+    rank() over (partition by product_id order by change_date desc) as rnk 
+    from products 
+    where change_date <= '2019-08-16'
+)
+select product_id, new_price as price 
+from latest_prices
+where rnk = 1
+union 
+select product_id, 10 as price 
+from products 
+where product_id not in (select distinct product_id from latest_prices)
