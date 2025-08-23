10ec459dad3a202b23077a58ed2bb515651dc50f
Venkyy97 <159583041+Venkyy97@users.noreply.github.com>
2025-08-22 20:06:17 -0500

    Time: 416 ms (97.17%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,14 +1,16 @@
-with latest_prices as 
-(
-    select *,
-    rank() over (partition by product_id order by change_date desc) as rnk 
+with cte as (
+    select *, 
+    rank() over (partition by product_id order by change_date desc ) as rnk 
     from products 
     where change_date <= '2019-08-16'
 )
-select product_id, new_price as price 
-from latest_prices
-where rnk = 1
+select product_id, new_price as price
+from cte 
+where rnk = 1 
 union 
-select product_id, 10 as price 
-from products 
-where product_id not in (select distinct product_id from latest_prices)
+select product_id, 10 as price
+from products
+where product_id not in (
+    select product_id 
+    from cte 
+)
